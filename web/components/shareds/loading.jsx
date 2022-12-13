a8815b88b6edbe5d86437d926b142967303cce8c
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-12-12 20:22:48 -1100

    feat-loading

---
+++ web/components/shareds/loading.jsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/loading.module.scss";
+
+
+const Loading = () => {
+    return (
+        <div className={styles.containerLoading}>
+            <span>Cargando...</span>
+        </div>
+
+    )
+}
+
+export default Loading
--- web/contextApi/context.jsx
+++ web/contextApi/context.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
+import Loading from "../components/shareds/loading";
 
 export const contextApi = createContext();
 
@@ -9,12 +10,14 @@ export const usePortfolio = () => {
 
 export const ProviderComponent = ({ children }) => {
   const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("http://localhost:5000/portafolio").then((res) => {
       setList(res.data.body);
+      setLoading(false);
     });
   }, [setList]);
 
-  return <contextApi.Provider value={{ list }}>{children}</contextApi.Provider>;
-};
+  return <contextApi.Provider value={{ list, loading }}>{loading && <Loading />}{children}</contextApi.Provider>
+}
