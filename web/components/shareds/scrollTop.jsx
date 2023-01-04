2c2159c399ae2ff2de20151ac4b5a8d856e275e5
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-01-03 22:43:32 -1100

    fix-refactor bug

--- web/components/shareds/scrollTop.jsx
+++ web/components/shareds/scrollTop.jsx
@@ -1,19 +1,22 @@
 import styles from "../../styles/scrollTop.module.scss";
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 
 const ScrollTop = () => {
-    const ref = useRef(null);
+    
+    const[scroll ,setScroll] =useState(false)
 
     const handleClick = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
     useEffect(() => {
-        const handleScroll = () => {
+        const handleScroll = () => {           
+
             if (window.pageYOffset > 1000) {
-                ref.current.style.display = 'grid';
+                setScroll(true)
+                
             } else {
-                ref.current.style.display = 'none';
+              setScroll(false)
             }
         };
 
@@ -26,9 +29,12 @@ const ScrollTop = () => {
 
     return (
         <>
-            <div ref={ref} onClick={handleClick} className={styles.containerTop}>
+            { scroll &&
+                <div  onClick={handleClick} className={styles.containerTop}>
                 <i className="bi bi-arrow-up"></i>
             </div>
+            }        
+            
         </>
     )
 }
