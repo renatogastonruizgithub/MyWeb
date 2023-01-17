fd8a0bcea0606c9e0ff084d656b8806514f323ad
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-01-16 22:36:41 -1100

    fix-refactor stilos del boton y hover

--- web/components/shareds/buton.jsx
+++ web/components/shareds/buton.jsx
@@ -1,5 +1,5 @@
 import styles from "../../styles/button.module.scss";
-const Botn = ({ children, color, background, font ,handleClick }) => {
+const Botn = ({ children, color, background, font, handleClick }) => {
 
 
     const styleProps = {
@@ -10,6 +10,9 @@ const Botn = ({ children, color, background, font ,handleClick }) => {
     return (
         <button onClick={handleClick} type="submit" className={styles.containerButton} style={styleProps}>
             {children}
+            <svg stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-linejoin="round" stroke-linecap="round"></path>
+            </svg>
         </button>
 
     )
